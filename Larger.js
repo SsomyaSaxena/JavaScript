38ae3521524cfb67b827514cbc1c6d01ad35d1e0
SsomyaSaxena <55494310+SsomyaSaxena@users.noreply.github.com>
2019-09-25 22:40:16 +0530

    Add files via upload

---
+++ Larger.js
@@ -0,0 +1,10 @@
+var num1 = prompt("Please enter a number", "0");
+var num2 = prompt("Please enter second number","0")
+if(num1 > num2)
+  {
+    console.log("FIRST number is larger",num1);
+  }
+else
+  {
+    console.log("SECOND number is larger",num2);
+  }
