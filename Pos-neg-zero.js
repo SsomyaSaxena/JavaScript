45ead513ae82dc4665e628f4463a3ec54a16dacb
SsomyaSaxena <55494310+SsomyaSaxena@users.noreply.github.com>
2019-09-25 22:23:42 +0530

    Add files via upload

---
+++ Pos-neg-zero.js
@@ -0,0 +1,13 @@
+var num = prompt("Please enter a number", "1");
+if(num==0)
+  {
+    console.log("your entered number is ZERO");
+  }
+if(num>0)
+  {
+    console.log("your entered number is POSITIVE");
+  }
+if(num<0)
+  {
+    console.log("your entered number is NEGATIVE");
+  }
